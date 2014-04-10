a40543cf10d90cc24b1578b56aa340b535e8d306
Jad Osseiran <jad6@icloud.com>
2014-04-10 10:14:57 +0800

    Initial Commit

---
+++ Jad's CV/CVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CVAppDelegate.h
+//  Jad's CV
+//
+//  Created by Jad Osseiran on 10/04/2014.
+//  Copyright (c) 2014 Jad Osseiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jad's CV/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jad's CV
+//
+//  Created by Jad Osseiran on 10/04/2014.
+//  Copyright (c) 2014 Jad Osseiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CVAppDelegate class]));
+    }
+}
